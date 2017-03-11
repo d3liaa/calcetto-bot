0bbf9f5fdcd46254c11dcc376d0986a712e4a7ff
Nadia Dillon <dillon.nadia@gmail.com>
2017-03-11 14:20:59 +0100

    test: create mock data and add tests for start, register and go methods

--- test/test.js
+++ test/test.js
@@ -5,44 +5,111 @@ const sinon = require('sinon');
 const chai = require('chai');
 const should = chai.should();
 const TournamentBot = require('../controllers');
+const mocks = require('./mocks');
 
 const bot = new TournamentBot();
-const msg = {
-  message_id: 1545,
-  from: { id: 306946885, first_name: 'Nadia', username: 'nadiadillon' },
-  chat: {
-    id: -113251997,
-    title: 'BOT',
-    type: 'group',
-    all_members_are_administrators: true
-  },
-  date: 1489160360,
-  text: '/start',
-  entities: [ { type: 'bot_command', offset: 0, length: 6 } ]
-};
 
-describe('Start', function ()  {
-  it('should add a tournament to chatsOpen', function (done) {
-    const chatId = msg.chat.id;
-    const getChatAdministrators = sinon.stub(bot.telegram, 'getChatAdministrators');
-    const res = [
-      {
+const createMessage = (user, chatId) => {
+  return {
+    message_id: user.msg_id,
+    from: { id: user.id, first_name: user.first_name, username: user.username },
+    chat: {
+      id: chatId,
+      title: 'BOT',
+      type: 'group',
+      all_members_are_administrators: true
+    },
+    date: 1489160360,
+    text: '/start',
+    entities: [ { type: 'bot_command', offset: 0, length: 6 } ]
+  }
+}
+
+mocks.forEach(chat => {
+  chat.users.forEach((user, index, arr) => {
+    arr[index] = createMessage(user, chat.chatId);
+  });
+});
+
+describe('Tournament Methods', function ()  {
+  let getChatAdministrators;
+  let sendMessage;
+
+  beforeEach(function () {
+    getChatAdministrators = sinon.stub(bot.telegram, 'getChatAdministrators');
+    sendMessage = sinon.stub(bot.telegram, 'sendMessage');
+
+  });
+
+  afterEach(function () {
+    getChatAdministrators.restore();
+    sendMessage.restore()
+  });
+
+  describe('start()', function () {
+    const chatAdmins = mocks.map(chat => chat.users[0]);
+    const res = chatAdmins.map(admin => {
+      return [{
         user: {
-          id: 306946885,
-          first_name: 'Nadia',
-          username: 'nadiadillon'
+          id: admin.from.id,
+          first_name: admin.from.first_name,
+          username: admin.from.username,
         },
         status: 'creator'
-      }
-    ];
-    const sendMessage = sinon.stub(bot.telegram, 'sendMessage');
+      }]
+    });
 
-    getChatAdministrators.returns(new Promise((resolve, reject) => resolve(res)));
-    bot.start(msg).then(() => {
-      bot.chatsOpen.should.have.property(chatId);
+  it(`should add 1 tournament to chatsOpen`, function (done) {
+    getChatAdministrators.returns(new Promise((resolve, reject) => resolve(res[0])));
+    bot.start(chatAdmins[0]).then(() => {
+      bot.chatsOpen.should.have.property(mocks[0].chatId);
       done();
-      getChatAdministrators.restore();
-      sendMessage.restore()
+    });
+  });
+
+  it(`should add multiple tournaments to chatsOpen `, function (done) {
+    getChatAdministrators.returns(new Promise((resolve, reject) => resolve(res[1])));
+    bot.start(chatAdmins[1]).then(() => {
+      bot.chatsOpen.should.have.property(mocks[1].chatId);
+      done();
+    });
+  })
+});
+
+  describe('register()', function () {
+    it('should add a player to the tournament', function () {
+      let tournament;
+      mocks.forEach(chat => {
+        tournament = bot.chatsOpen[chat.chatId]
+        chat.users.forEach(msg => {
+          let username = msg.from.username;
+          bot.register(msg)
+          tournament.players.should.have.property(username);
+        })
+      })
+      tournament.playing.should.be.false;
+      tournament.registering.should.be.true;
+    });
+  });
+
+  describe('go()', function () {
+    const chatAdmins = mocks.map(chat => chat.users[0]);
+
+    it('should start a tournament with 4 players or more', function () {
+      let tournament;
+      chatAdmins.forEach((admin, i) => {
+        const chatId = mocks[i].chatId
+        tournament = bot.chatsOpen[chatId];
+        bot.go(admin)
+        tournament.registering.should.be.false;
+        tournament.playing.should.be.true;
+        Object.keys(tournament.playingPlayers).length.should.eql(Object.keys(tournament.players).length);
+      });
+    });
+
+  describe('result', function () {
+    it('should update the current game with the scores', function () {
+
     });
   });
 });
