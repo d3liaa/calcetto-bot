7bb6b639203ac24dc3b8693837bdb80ecbb1ce58
Jonathan Wieben <JonathanWieben@Jonathans-MacBook-Air.local>
2017-03-13 15:38:51 +0100

    feat: add renderJSON function

--- d3/renderJSON.js
+++ d3/renderJSON.js
@@ -2,85 +2,79 @@
 
 const fs = require('fs');
 
-const mockRounds = [
-  [
-    {
-      player1: { name: 'a' },
-      player2: { name: 'b' }
-    },
-    {
-      player1: 0,
-      player2: 0
-    },
-    {
-      player1: { name: 'd' },
-      player2: { name: 'c' }
-    },
-    {
-      player1: 0,
-      player2: { name: 'e' }
-    },
-  ],
-  [
-    {
-      player1: { name: 'a' },
-      player2: 0
-    },
-    {
-      player1: { name: 'd' },
-      player2: { name: 'e' }
-    },
-  ],
-  [
-    {
-      player1: { name: 'a' },
-      player2: null
-    }
-  ],
-  null
-];
-
 const renderJSON = (rounds) => {
-
-  // create object with based on rounds
-
-
-
   const bracket = {};
-  const putInBracket = (str) => {
-    let parentBracket = bracket;
+
+  // inserts a string at next possible place in bracket
+  // TODO: only balanced up to 8 players
+  const insert = (str) => {
     let currentBracket = bracket.winners;
     let pushed = false;
     while (!pushed) {
       if (currentBracket.length < 2) {
-        currentBracket.push({ name: str, winners: [] });
+        currentBracket.push({ 'name': str, 'winners': [] });
         pushed = true;
       } else {
-
+        if (currentBracket[0].winners.length < 2) {
+          currentBracket = currentBracket[0].winners;
+        } else if (currentBracket[1].winners.length < 2) {
+          currentBracket = currentBracket[1].winners;
+        } else {
+          if (currentBracket[0].winners[0].winners.length < 2) {
+            currentBracket = currentBracket[0].winners[0].winners;
+          } else if  (currentBracket[0].winners[1].winners.length < 2) {
+            currentBracket = currentBracket[0].winners[1].winners;
+          } else if  (currentBracket[1].winners[0].winners.length < 2) {
+            currentBracket = currentBracket[1].winners[0].winners;
+          } else if  (currentBracket[1].winners[1].winners.length < 2) {
+            currentBracket = currentBracket[1].winners[1].winners;
+          }
+        }
       }
-      // if (currentBracket.length < 2) {
-      //   currentBracket.push({ 'name': str, 'winners': [] });
-      //   pushed = true;
-      // } else {
-      //   if (currentBracket[0].winners.length < 2) {
-      //     currentBracket = currentBracket[0].winners;
-      //   } else if (currentBracket[1].winners.length < 2) {
-      //     currentBracket = currentBracket[1].winners;
-      //   }
-      // }
     };
   };
 
+  // removes the games that have a 0 in them
+  const removeZeros = (bracket) => {
+    if (bracket.winners.length === 2) {
+      if (bracket.winners[1].name === '0') {
+        bracket.winners.splice(1,1);
+      } else removeZeros(bracket.winners[1]);
+      if (bracket.winners[0].name === '0') {
+        bracket.winners.splice(0,1);
+      } else removeZeros(bracket.winners[0]);
+    } else if (bracket.winners.length === 1) {
+      if (bracket.winners[0].name === '0') bracket.winners.splice(0,1);
+      else removeZeros(bracket.winners[0]);
+    }
+  };
+
+  // iterates over the rounds array
+  // and inserts each game into bracket
   for (let i = rounds.length-1; i >= 0; i--) {
     if (Array.isArray(rounds[i])) {
       for (let j = 0; j < rounds[i].length; j++) {
         const game = rounds[i][j];
-        if (game.player1) putInBracket(game.player1.name);
-        if (game.player1 === null) putInBracket('null');
-        if (game.player1 === 0) putInBracket('0');
-        if (game.player2) putInBracket(game.player2.name);
-        if (game.player2 === null) putInBracket('null');
-        if (game.player2 === 0) putInBracket('0');
+        switch (game.player1) {
+          case null:
+            insert('');
+            break;
+          case 0:
+            insert('0');
+            break;
+          default:
+            insert(game.player1.name);
+        };
+        switch (game.player2) {
+          case null:
+            insert('');
+            break;
+          case 0:
+            insert('0');
+            break;
+          default:
+            insert(game.player2.name);
+        };
       };
     } else {
       if (rounds[i]) bracket.name = player.name;
@@ -89,11 +83,12 @@ const renderJSON = (rounds) => {
     }
   };
 
+  removeZeros(bracket);
+
   fs.writeFile('./d3/bracket.json', JSON.stringify(bracket), err => {
     if (err) console.log(err);
   });
-};
 
-renderJSON(mockRounds);
+};
 
 module.exports = renderJSON;
