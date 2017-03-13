05399a084d19ee4d7a277a4cd3da55197bff17a3
Nadia Dillon <dillon.nadia@gmail.com>
2017-03-13 13:36:07 +0100

    fix: unused code that is conflicting with tests

--- d3/renderJSON.js
+++ d3/renderJSON.js
@@ -58,36 +58,19 @@ const renderJSON = (rounds) => {
       } else {
 
       }
-      // if (currentBracket.length < 2) {
-      //   currentBracket.push({ 'name': str, 'winners': [] });
-      //   pushed = true;
-      // } else {
-      //   if (currentBracket[0].winners.length < 2) {
-      //     currentBracket = currentBracket[0].winners;
-      //   } else if (currentBracket[1].winners.length < 2) {
-      //     currentBracket = currentBracket[1].winners;
-      //   }
-      // }
+      if (currentBracket.length < 2) {
+        currentBracket.push({ 'name': str, 'winners': [] });
+        pushed = true;
+      } else {
+        if (currentBracket[0].winners.length < 2) {
+          currentBracket = currentBracket[0].winners;
+        } else if (currentBracket[1].winners.length < 2) {
+          currentBracket = currentBracket[1].winners;
+        }
+      }
     };
   };
 
-  for (let i = rounds.length-1; i >= 0; i--) {
-    if (Array.isArray(rounds[i])) {
-      for (let j = 0; j < rounds[i].length; j++) {
-        const game = rounds[i][j];
-        if (game.player1) putInBracket(game.player1.name);
-        if (game.player1 === null) putInBracket('null');
-        if (game.player1 === 0) putInBracket('0');
-        if (game.player2) putInBracket(game.player2.name);
-        if (game.player2 === null) putInBracket('null');
-        if (game.player2 === 0) putInBracket('0');
-      };
-    } else {
-      if (rounds[i]) bracket.name = player.name;
-      else bracket.name = '';
-      bracket.winners = [];
-    }
-  };
 
   fs.writeFile('./d3/bracket.json', JSON.stringify(bracket), err => {
     if (err) console.log(err);
